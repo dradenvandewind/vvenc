1fda8e7ccdb46531df85e1bdbda3c57d6c7ece8a
le blond erwan <erwanleblond@gmail.com>
2025-12-09 18:53:03 +0100

    try fix

--- source/Lib/vvenc/vvencCfg.cpp
+++ source/Lib/vvenc/vvencCfg.cpp
@@ -810,8 +810,6 @@ VVENC_DECL bool vvenc_init_config_parameter( vvenc_config *c )
 
   vvenc_confirmParameter( c,  (c->m_numIntraModesFullRD < -1 || c->m_numIntraModesFullRD == 0 || c->m_numIntraModesFullRD > 3), "NumIntraModesFullRD must be -1 or between 1 and 3");
 
-  vvenc_confirmParameter( c, c->m_vpsId[0] < 0 || c->m_vpsId[14] > 15 ,                                           "Configure VpsId" );
-  //vvenc_confirmParameter( c, c->m_forceVpsOutput[0] < 0 || c->m_forceVpsOutput[1] > 1 ,                         "Force Vps in Output Stream" );
 
 
 
